4c9b7ac7278aaeba3deaeafbfc9c94632cf553a3
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-05-24 15:41:31 +0200

    styles for rodo imput and btn

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const formInputs = document.querySelectorAll(".form__input");
 const formAlerts = document.querySelectorAll(".form-alert");
-
-// document.getElementById("name").addEventListener("focus", function () {
-//   document.querySelector(".form-alert").style.visibility = "visible";
-//   document.querySelector(".form-alert").style.height = "fit-content";
-// });
-
-// document.getElementById("name").addEventListener("blur", function () {
-//   document.querySelector(".form-alert").style.visibility = "hidden";
-//   document.querySelector(".form-alert").style.height = "0";
-// });
-
+const submitBtn = document.querySelector(".submit-btn");
 formInputs.forEach((input, i) => {
   input.addEventListener("focus", () => {
-    formAlerts[i].style.visibility = "visible";
-    formAlerts[i].style.height = "fit-content";
+    formAlerts[i].style.opacity = "1";
+    if (formAlerts[i] === formAlerts[2]) {
+      formAlerts[i].style.height = "4rem";
+    } else {
+      formAlerts[i].style.height = "2rem";
+    }
   });
 });
 formInputs.forEach((input, i) => {
   input.addEventListener("blur", () => {
-    formAlerts[i].style.visibility = "hidden";
+    formAlerts[i].style.opacity = "0";
     formAlerts[i].style.height = "0";
   });
+});
+
+const validateName = (e) => {
+  e.preventDefault();
+  console.log(formInputs[0].value);
+  console.log(formInputs[0].value.length >= 2);
+  if (formInputs[0].value.length >= 2) {
+    console.log("error");
+  }
+};
+
+submitBtn.addEventListener("click", validateName);
+formInputs[0].addEventListener("keyup", () => {
+  console.log(formAlerts[0].firstElementChild);
+  if (formInputs[0].value.length >= 2) {
+    formAlerts[0].firstElementChild.style.color = "greenyellow";
+    formAlerts[0].firstElementChild.textContent += " ✅";
+  }
+  if (formInputs[0].value.includes("@")) {
+    formAlerts[0].lastElementChild.style.color = "tomato";
+    console.log("ok");
+  } else {
+    console.log("not ok");
+    formAlerts[0].lastElementChild.style.color = "greenyellow";
+  }
 });
