377a9a68fa9ba3986286649f59d9711b3aa99464
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-05-24 23:57:42 +0200

    validation for inputs

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 const formInputs = document.querySelectorAll(".form__input");
+const nameInput = document.querySelector(".input-name");
+const emailInput = document.querySelector(".input-email");
+const passwordInput = document.querySelector(".input-password");
+const confirmPasswordInput = document.querySelector(".input-confirm-password");
 const formAlerts = document.querySelectorAll(".form-alert");
 const submitBtn = document.querySelector(".submit-btn");
+
+let passwordValue;
+
+const inputErrors = [
+  {
+    name: {
+      errorOne: "at least 2 signs",
+      errorTwo: "not numbers and special characters",
+    },
+  },
+  {
+    email: {
+      errorOne: "must contain @ and domainat least 2 signs",
+    },
+  },
+  {
+    password: {
+      errorOne: "at least 2 signs",
+      errorTwo: "at least 8 characters",
+      errorThree: "at least one digit",
+      errorFour: "at least one special character",
+    },
+  },
+  {
+    confirmPassword: {
+      errorOne: "must be the same like password",
+    },
+  },
+  {
+    rodo: {
+      errorOne: "RODO must be accepted",
+    },
+  },
+];
+
 formInputs.forEach((input, i) => {
   input.addEventListener("focus", () => {
     formAlerts[i].style.opacity = "1";
@@ -18,27 +57,60 @@ formInputs.forEach((input, i) => {
   });
 });
 
-const validateName = (e) => {
-  e.preventDefault();
-  console.log(formInputs[0].value);
-  console.log(formInputs[0].value.length >= 2);
-  if (formInputs[0].value.length >= 2) {
-    console.log("error");
-  }
-};
-
-submitBtn.addEventListener("click", validateName);
-formInputs[0].addEventListener("keyup", () => {
-  console.log(formAlerts[0].firstElementChild);
-  if (formInputs[0].value.length >= 2) {
+nameInput.addEventListener("keyup", () => {
+  if (nameInput.value.length >= 2) {
     formAlerts[0].firstElementChild.style.color = "greenyellow";
-    formAlerts[0].firstElementChild.textContent += " ✅";
-  }
-  if (formInputs[0].value.includes("@")) {
-    formAlerts[0].lastElementChild.style.color = "tomato";
-    console.log("ok");
   } else {
-    console.log("not ok");
+    formAlerts[0].firstElementChild.style.color = "tomato";
+  }
+  if (nameInput.value.includes("@")) {
+    formAlerts[0].lastElementChild.style.color = "tomato";
+  } else {
     formAlerts[0].lastElementChild.style.color = "greenyellow";
+  }
+});
+
+emailInput.addEventListener("keyup", () => {
+  console.log(formAlerts[1].textContent);
+  if (emailInput.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+    formAlerts[1].style.color = "greenyellow";
+  } else {
+    formAlerts[1].style.color = "tomato";
+  }
+});
+
+passwordInput.addEventListener("keyup", () => {
+  if (passwordInput.value.length >= 8) {
+    formAlerts[2].firstElementChild.style.color = "greenyellow";
+  } else {
+    formAlerts[2].firstElementChild.style.color = "tomato";
+  }
+  if (passwordInput.value.match(/[A-Z]/)) {
+    formAlerts[2].firstElementChild.nextElementSibling.style.color =
+      "greenyellow";
+  } else {
+    formAlerts[2].firstElementChild.nextElementSibling.style.color = "tomato";
+  }
+  if (passwordInput.value.match(/[0-9]/)) {
+    formAlerts[2].lastElementChild.previousElementSibling.style.color =
+      "greenyellow";
+  } else {
+    formAlerts[2].lastElementChild.previousElementSibling.style.color =
+      "tomato";
+  }
+  if (passwordInput.value.match(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/)) {
+    formAlerts[2].lastElementChild.style.color = "greenyellow";
+  } else {
+    formAlerts[2].lastElementChild.style.color = "tomato";
+  }
+  return passwordInput.value;
+});
+
+confirmPasswordInput.addEventListener("keyup", () => {
+  console.log(passwordInput.value, confirmPasswordInput.value);
+  if (confirmPasswordInput.value === passwordInput.value) {
+    formAlerts[3].style.color = "yellowgreen";
+  } else {
+    formAlerts[3].style.color = "tomato";
   }
 });
---
+++ src/scripts/validation.js
@@ -0,0 +1,5 @@
+export const validation = (element, rule) => {
+  if (rule) {
+    element.style.color = "yellowgreen";
+  }
+};
