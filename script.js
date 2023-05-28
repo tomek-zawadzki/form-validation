31ec2e62b866bfcac3ae234f0894ae5402fd8b67
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-05-28 18:59:28 +0200

    group some variabels, and used then to refactor createErrorMSg function

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-const formInputs = document.querySelectorAll(".form__input");
-const nameInput = document.querySelector(".input-name");
-const emailInput = document.querySelector(".input-email");
-const passwordInput = document.querySelector(".input-password");
-const confirmPasswordInput = document.querySelector(".input-confirm-password");
-const rodoInput = document.querySelector(".input-rodo");
-const alertNameBox = document.querySelector(".form-alert-name");
-const alertEmailBox = document.querySelector(".form-alert-email");
-const alertPasswordBox = document.querySelector(".form-alert-password");
-const alertConfirmPasswordBox = document.querySelector(
-  ".form-alert-confirm-password"
-);
-const alertRodoBox = document.querySelector(".form-alert-rodo");
-const formAlerts = document.querySelectorAll(".form-alert");
+const fields = {
+  name: document.querySelector(".input-name"),
+  email: document.querySelector(".input-email"),
+  password: document.querySelector(".input-password"),
+  confirmPassword: document.querySelector(".input-confirm-password"),
+  rodo: document.querySelector(".input-rodo"),
+};
+const alertBoxes = {
+  name: document.querySelector(".form-alert-name"),
+  email: document.querySelector(".form-alert-email"),
+  password: document.querySelector(".form-alert-password"),
+  confirmPassword: document.querySelector(".form-alert-confirm-password"),
+  rodo: document.querySelector(".form-alert-rodo"),
+};
+const formInputs = Array.from(document.querySelectorAll(".form__input"));
+const formAlerts = Array.from(document.querySelectorAll(".form-alert"));
 const sendBtn = document.querySelector(".submit-btn");
-const elementBox = document.querySelectorAll(".element__box");
 
 const inputErrors = {
   name: [
     {
       text: "at least 2 signs",
-      validator: () => nameInput.value.length >= 2,
+      validator: () => fields.name.value.length >= 2,
       id: 0,
     },
     {
       text: "without numbers and special characters",
       validator: () =>
-        !nameInput.value.match(/[0-9`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/),
+        !fields.name.value.match(/[0-9`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/),
       id: 1,
     },
   ],
@@ -33,30 +34,30 @@ const inputErrors = {
     {
       text: "must contain @ and domainat",
       validator: () =>
-        emailInput.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g),
+        fields.email.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g),
       id: 2,
     },
   ],
   password: [
     {
       text: "at least 8 characters",
-      validator: () => passwordInput.value.length >= 8,
+      validator: () => fields.password.value.length >= 8,
       id: 3,
     },
     {
       text: "at least one uppercased character",
-      validator: () => passwordInput.value.match(/[A-Z]/),
+      validator: () => fields.password.value.match(/[A-Z]/),
       id: 4,
     },
     {
       text: "at least one digit",
-      validator: () => passwordInput.value.match(/[0-9]/),
+      validator: () => fields.password.value.match(/[0-9]/),
       id: 5,
     },
     {
       text: "at least one special character",
       validator: () =>
-        passwordInput.value.match(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/),
+        fields.password.value.match(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/),
       id: 6,
     },
   ],
@@ -64,8 +65,8 @@ const inputErrors = {
     {
       text: "must be the same like password",
       validator: () =>
-        confirmPasswordInput.value === passwordInput.value &&
-        passwordInput.value !== "",
+        fields.confirmPassword.value === fields.password.value &&
+        fields.password.value !== "",
       id: 7,
     },
   ],
@@ -86,16 +87,6 @@ const createErrorMsg = (element, boxToPushMsg) => {
   });
 };
 
-Object.keys(inputErrors).forEach((key) => {
-  inputErrors[key].forEach((el) => {
-    const errorMsg = document.createElement("span");
-    errorMsg.classList.add("error-msg");
-    errorMsg.textContent = el.text;
-    // console.log(errorMsg.textContent);
-  });
-});
-// console.log(Object.entries(inputErrors));
-
 formInputs.forEach((input, i) => {
   const eventHandler = (opacity, height) => {
     formAlerts[i].style.opacity = opacity;
@@ -115,35 +106,29 @@ function validateInput(element, validator) {
   }
 }
 
-createErrorMsg(inputErrors.name, alertNameBox);
-createErrorMsg(inputErrors.email, alertEmailBox);
-createErrorMsg(inputErrors.password, alertPasswordBox);
-createErrorMsg(inputErrors.confirmPassword, alertConfirmPasswordBox);
-createErrorMsg(inputErrors.rodo, alertRodoBox);
+for (const fieldName in inputErrors) {
+  createErrorMsg(inputErrors[fieldName], alertBoxes[fieldName]);
+}
 
 const errorMsgs = document.querySelectorAll(".error-msg");
 
-const inputs = [nameInput, emailInput, passwordInput, confirmPasswordInput];
-
-inputs.forEach((input) => {
-  input.addEventListener("keyup", () => {
-    for (const field in inputErrors) {
-      inputErrors[field].forEach((error) => {
+for (const field in fields) {
+  fields[field].addEventListener("keyup", () => {
+    for (const errorField in inputErrors) {
+      inputErrors[errorField].forEach((error) => {
         if (!error.validator) return;
         validateInput(errorMsgs[error.id], error.validator());
       });
     }
   });
-});
+}
 
-const validateRodo = () => {
-  alertRodoBox.firstElementChild.classList.toggle(
+fields.rodo.addEventListener("change", () => {
+  alertBoxes.rodo.firstElementChild.classList.toggle(
     "form-alert-correct",
-    rodoInput.checked
+    fields.rodo.checked
   );
-};
-
-rodoInput.addEventListener("change", validateRodo);
+});
 
 const checkFormIsValid = (e) => {
   e.preventDefault();
